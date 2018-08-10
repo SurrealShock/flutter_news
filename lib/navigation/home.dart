b07176748f9905f21ce234f813327e5a38b0ac39
SurrealShock <Adrian_Avram@Outlook.com>
2018-08-10 08:29:48 -0700

    Added collapsing appBar

--- lib/navigation/home.dart
+++ lib/navigation/home.dart
@@ -26,15 +26,14 @@ class HomeState extends State<Home> {
   var data = Map();
 
   void bookMark(BookMarkItem bMrkItm) async {
-
     reference.push().set(bMrkItm.toJson());
   }
 
-  // @override
-  // void dispose() {
-  //   controller.removeListener(_scrollListener);
-  //   super.dispose();
-  // }
+  @override
+  void dispose() {
+    // controller.removeListener(_scrollListener);
+    super.dispose();
+  }
 
   // _scrollListener() {
   //   print(controller.position.extentAfter);
@@ -43,161 +42,160 @@ class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('News'),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {
-              Navigator
-                  .of(context)
-                  .push(MaterialPageRoute(builder: (context) => Search()));
-            },
-            icon: Icon(Icons.search),
-          ),
-          StreamBuilder(
-            stream: FirebaseAuth.instance.currentUser().asStream(),
-            builder:
-                (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
-              if (snapshot.hasData) {
-                return PopupMenuButton(
-                  child: ProfilePicture(snapshot.data.photoUrl),
-                  onSelected: (index) {
-                    switch (index) {
-                      case 0:
-                        Auth.signOutFirebase();
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (BuildContext context) => MyApp()));
-                        break;
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+              title: Text("News"),
+              centerTitle: true,
+              floating: true,
+              snap: true,
+              elevation: 4.0,
+              actions: <Widget>[
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Search()));
+                  },
+                  icon: Icon(Icons.search),
+                ),
+                StreamBuilder(
+                  stream: FirebaseAuth.instance.currentUser().asStream(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<FirebaseUser> snapshot) {
+                    if (snapshot.hasData) {
+                      return PopupMenuButton(
+                        child: profilePicture(snapshot.data.photoUrl),
+                        onSelected: (index) {
+                          switch (index) {
+                            case 0:
+                              Auth.signOutFirebase();
+                              Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                          MyApp()));
+                              break;
+                          }
+                        },
+                        itemBuilder: (context) {
+                          return <PopupMenuEntry<int>>[
+                            PopupMenuItem(
+                              value: 0,
+                              child: Text('Sign out'),
+                            )
+                          ];
+                        },
+                      );
+                    } else {
+                      return Container(
+                        height: 0.0,
+                        width: 0.0,
+                      );
                     }
                   },
-                  itemBuilder: (context) {
-                    return <PopupMenuEntry<int>>[
-                      PopupMenuItem(
-                        value: 0,
-                        child: Text('Sign out'),
-                      )
-                    ];
-                  },
+                ),
+              ]),
+          FutureBuilder(
+            future: getFromUrl.fetch(
+                "https://newsapi.org/v2/top-headlines?country=us&apiKey=" +
+                    apiKey),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                _loadNewsItem(snapshot);
+                return SliverList(
+                  // controller: controller,
+                  delegate: SliverChildBuilderDelegate((context, index) {
+                    return Dismissible(
+                      onDismissed: (direction) {
+                        bookMarkItem.removeAt(index);
+                      },
+                      key: Key(bookMarkItem[index].articleURL),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 8.0, right: 8.0, top: 6.0, bottom: 6.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            launchURL(bookMarkItem[index].articleURL);
+                          },
+                          child: NewsContainer(
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Row(children: <Widget>[
+                                NewsCard(
+                                    showDate: true,
+                                    bookMarkItem: bookMarkItem[index],
+                                    customPopUpMenu: PopupMenuButton<int>(
+                                      icon: Icon(
+                                        Icons.more_vert,
+                                        size: 20.0,
+                                        color: Colors.grey[700],
+                                      ),
+                                      padding: EdgeInsets.zero,
+                                      onSelected: (_) {
+                                        switch (_) {
+                                          case 0:
+                                            if (!data.containsKey(
+                                                bookMarkItem[index].title)) {
+                                              bookMark(bookMarkItem[index]);
+                                            } else {
+                                              BookMarkItem.removeBookMark(
+                                                  FirebaseDatabase.instance
+                                                      .reference()
+                                                      .child(
+                                                          'users/' + user.uid)
+                                                      .child('/' +
+                                                          data[bookMarkItem[
+                                                                  index]
+                                                              .title]));
+                                            }
+                                            break;
+                                          case 1:
+                                            break;
+                                        }
+                                      },
+                                      itemBuilder: (BuildContext context) {
+                                        bool bookmarked = data.containsKey(
+                                            bookMarkItem[index].title);
+                                        return <PopupMenuEntry<int>>[
+                                          PopupMenuItem<int>(
+                                            value: 0,
+                                            child: Row(
+                                              children: <Widget>[
+                                                Icon(bookmarked
+                                                    ? Icons.bookmark
+                                                    : Icons.bookmark_border),
+                                                Text(bookmarked
+                                                    ? "Remove bookmark"
+                                                    : "  Bookmark")
+                                              ],
+                                            ),
+                                          ),
+                                        ];
+                                      },
+                                    ))
+                              ]),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }, childCount: snapshot.data['totalResults']),
                 );
               } else {
-                return Container(
-                  height: 0.0,
-                  width: 0.0,
+                return SliverList(
+                  delegate: SliverChildBuilderDelegate((context, index) {
+                    return LoadingCard();
+                  }, childCount: 10),
                 );
               }
             },
-          ),
+          )
         ],
-      ),
-      body: FutureBuilder(
-        future: getFromUrl.fetch(
-            "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + apiKey),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data['totalResults'],
-              itemBuilder: (context, index) {
-                bookMarkItem.add(
-                    BookMarkItem.fromJson(snapshot.data['articles'][index]));
-                return Dismissible(
-                  onDismissed: (direction) {
-                    bookMarkItem.removeAt(index);
-                  },
-                  key: Key(bookMarkItem[index].articleURL),
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 8.0, right: 8.0, top: 6.0, bottom: 6.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        launchURL(bookMarkItem[index].articleURL);
-                      },
-                      child: NewsContainer(
-                        child: Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Row(children: <Widget>[
-                            NewsCard(
-                              showDate: true,
-                                bookMarkItem: bookMarkItem[index],
-                                customPopUpMenu: PopupMenuButton<int>(
-                                  icon: Icon(
-                                    Icons.more_vert,
-                                    size: 20.0,
-                                    color: Colors.grey[700],
-                                  ),
-                                  padding: EdgeInsets.zero,
-                                  onSelected: (_) {
-                                    switch (_) {
-                                      case 0:
-                                        if (!data.containsKey(
-                                            bookMarkItem[index].title)) {
-                                          bookMark(bookMarkItem[index]);
-                                        } else {
-                                          BookMarkItem.removeBookMark(
-                                              FirebaseDatabase.instance
-                                                  .reference()
-                                                  .child('users/' + user.uid)
-                                                  .child('/' +
-                                                      data[bookMarkItem[index]
-                                                          .title]));
-                                        }
-                                        break;
-                                      case 1:
-                                        break;
-                                    }
-                                  },
-                                  itemBuilder: (BuildContext context) {
-                                    bool bookmarked = data
-                                        .containsKey(bookMarkItem[index].title);
-                                    return <PopupMenuEntry<int>>[
-                                      PopupMenuItem<int>(
-                                        value: 0,
-                                        child: Row(
-                                          children: <Widget>[
-                                            Icon(bookmarked
-                                                ? Icons.bookmark
-                                                : Icons.bookmark_border),
-                                            Text(bookmarked
-                                                ? "Remove bookmark"
-                                                : "  Bookmark")
-                                          ],
-                                        ),
-                                      ),
-                                      PopupMenuItem<int>(
-                                        value: 1,
-                                        child: Row(
-                                          children: <Widget>[
-                                            Icon(Icons.settings),
-                                            Text("  Customize")
-                                          ],
-                                        ),
-                                      ),
-                                    ];
-                                  },
-                                ))
-                          ]),
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            );
-          } else {
-            return ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return LoadingCard();
-                });
-          }
-        },
       ),
     );
   }
 
   @override
   void initState() {
-    super.initState();
     // controller = ScrollController()..addListener(_scrollListener);
     final FirebaseDatabase firebaseDatabase = FirebaseDatabase.instance;
     Auth.getUser().then((user) {
@@ -206,6 +204,13 @@ class HomeState extends State<Home> {
       reference.onChildAdded.listen(_onEntryAdded);
       reference.onChildRemoved.listen(_onEntryRemoved);
     });
+    super.initState();
+  }
+
+  _loadNewsItem(final snapshot) async {
+    for (var i = 0; i < 20; i++) {
+      bookMarkItem.add(BookMarkItem.fromJson(snapshot.data['articles'][i]));
+    }
   }
 
   _onEntryAdded(Event event) {
