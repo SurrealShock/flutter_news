e0cccd0bb3a53b14cef1476a2319079cefe817dc
SurrealShock <Adrian_Avram@Outlook.com>
2018-08-18 16:58:02 -0700

    Reorganized

--- lib/navigation/bookmarks.dart
+++ lib/navigation/bookmarks.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_news/main.dart';
-import 'package:flutter_news/navigation/search.dart';
 import 'package:flutter_news/navigation/widgets.dart';
 import 'package:flutter_news/utilities/bookMarkItem.dart';
 import 'package:flutter_news/utilities/firebase.dart';
